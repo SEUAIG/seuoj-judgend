a6ef3a1c78275b4b8c88d32da38fdf610403647c
harkerhand <harkerhand@outlook.com>
2025-12-18 21:08:30 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![deny(missing_docs)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+//! SEU AIJ Judge-Endpoint
+
+
+fn main() {
+    println!("Hello, aij!");
+}
